f7c304308d9ce7ea82eae6cd80e4bfebc2f60ac0
rupeshsasne <rupeshsasne@gmail.com>
2019-06-13 16:43:21 +0530

    rearrange the when branch ordering

--- classroom/src/main/kotlin/com/radix2/algorithms/week3/TraditionalMergeSort.kt
+++ classroom/src/main/kotlin/com/radix2/algorithms/week3/TraditionalMergeSort.kt
@@ -1,21 +1,21 @@
 package com.radix2.algorithms.week3
 
-fun IntArray.mergeSort(comparator: (Int, Int) -> Int) {
+fun IntArray.mergeSort() {
     val aux = IntArray(size)
-    sort(this, aux, 0, size - 1, comparator)
+    sort(this, aux, 0, size - 1)
 }
 
-fun sort(array: IntArray, aux: IntArray, lo: Int, hi: Int, comparator: (Int, Int) -> Int) {
+fun sort(array: IntArray, aux: IntArray, lo: Int, hi: Int) {
     if (lo >= hi) return
 
     val mid = lo + (hi - lo) / 2
 
-    sort(array, aux, lo, mid, comparator)
-    sort(array, aux, mid + 1, hi, comparator)
-    merge(array, aux, lo, mid, hi, comparator)
+    sort(array, aux, lo, mid)
+    sort(array, aux, mid + 1, hi)
+    merge(array, aux, lo, mid, hi)
 }
 
-fun merge(array: IntArray, aux: IntArray, lo: Int, mid: Int, hi: Int, comparator: (Int, Int) -> Int) {
+fun merge(array: IntArray, aux: IntArray, lo: Int, mid: Int, hi: Int) {
     System.arraycopy(array, lo, aux, lo, (hi - lo) + 1)
 
     var i = lo
@@ -25,14 +25,16 @@ fun merge(array: IntArray, aux: IntArray, lo: Int, mid: Int, hi: Int, comparator
         when {
             i > mid -> array[k] = aux[j++]
             j > hi -> array[k] = aux[i++]
-            comparator(aux[i], aux[j]) > 0 -> array[k] = aux[i++]
-            else -> array[k] = aux[j++]
+            aux[i] > aux[j] -> {
+                array[k] = aux[j++]
+            }
+            else -> array[k] = aux[i++]
         }
     }
 }
 
 fun main(args: Array<String>) {
     val array = intArrayOf(6, 2, 1, 0, 13, 89, 72, 4, 22, 28)
-    array.mergeSort { t1, t2 -> if (t1 < t2) -1 else 1 }
+    array.mergeSort()
     println(array.joinToString())
 }
