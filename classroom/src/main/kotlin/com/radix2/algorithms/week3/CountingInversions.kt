69822551851b6e652653f61037a5411f3c5ddf8e
rupeshsasne <rupeshsasne@gmail.com>
2019-06-13 16:55:53 +0530

    fix variable name

--- classroom/src/main/kotlin/com/radix2/algorithms/week3/CountingInversions.kt
+++ classroom/src/main/kotlin/com/radix2/algorithms/week3/CountingInversions.kt
@@ -20,7 +20,7 @@ fun sort(array: Array<Int>, aux: Array<Int>, lo: Int, hi: Int): Long {
 fun merge(array: Array<Int>, aux: Array<Int>, lo: Int, mid: Int, hi: Int): Long {
     var i = lo
     var j = mid + 1
-    val midToNext = j
+    val nextToMid = j
     var k = lo
 
     var inversions = 0L
@@ -30,7 +30,7 @@ fun merge(array: Array<Int>, aux: Array<Int>, lo: Int, mid: Int, hi: Int): Long 
             aux[k++] = array[i++]
         } else {
             aux[k++] = array[j++]
-            inversions += midToNext - i
+            inversions += nextToMid - i
         }
     }
 
