948831f514bb1b7b392f1e82c33103e5ba8e3f9d
rupeshsasne <rupeshsasne@gmail.com>
2019-06-29 20:26:43 +0530

    fix indexing issue

--- classroom/src/main/kotlin/com/radix2/algorithms/week4/MaxPQ.kt
+++ classroom/src/main/kotlin/com/radix2/algorithms/week4/MaxPQ.kt
@@ -1,59 +1,21 @@
 package com.radix2.algorithms.week4
 
 class MaxPQ<T : Comparable<T>>(capacity: Int) {
-    private var array = Array<Comparable<Any>?>(capacity + 1) { null }
+    private var array = Array<Any?>(capacity + 1) { null }
 
     private var n = 0
 
-    companion object {
-
-        private fun sink(array: Array<Comparable<Any>>, k: Int, n: Int) {
-            var trav = k
-
-            while (2 * trav <= n) {
-                var j = 2 * trav
-
-                if (j < n && less(array, j, j + 1))
-                    j++
-
-                if (!less(array, k, j)) break
-
-                exch(array, k, j)
-
-                trav = j
-            }
-        }
-
-        private fun swim(array: Array<Comparable<Any>>, k: Int) {
-            var trav = k
-
-            while (trav > 1 && less(array,trav / 2, trav)) {
-                exch(array, trav, trav / 2)
-                trav /= 2
-            }
-        }
-
-        private fun exch(array: Array<Comparable<Any>>, i: Int, j: Int) {
-            val temp = array[i]
-            array[i] = array[j]
-            array[j] = temp
-        }
-
-        private fun less(array: Array<Comparable<Any>>, lhs: Int, rhs: Int): Boolean = array[lhs] < (array[rhs])
-    }
-
-    @Suppress("UNCHECKED_CAST")
     fun add(t: T) {
-        array[++n] = t as Comparable<Any>
-        swim(array as Array<Comparable<Any>>, n)
+        array[++n] = t
+
+        swim(n)
     }
 
-    @Suppress("UNCHECKED_CAST")
     fun deleteMax(): T {
         val key = array[1]
 
-        exch(array as Array<Comparable<Any>>, 1, n--)
-        sink(array as Array<Comparable<Any>>, 1, n)
+        exch(1, n--)
+        sink(1)
         array[n + 1] = null
 
         @Suppress("UNCHECKED_CAST")
@@ -62,15 +24,49 @@ class MaxPQ<T : Comparable<T>>(capacity: Int) {
 
     fun size() = n
 
+    private fun sink(k: Int) {
+        var trav = k
+
+        while (2 * trav <= n) {
+            var j = 2 * trav
+
+            if (j < n && less(j, j + 1))
+                j++
+
+            if (!less(trav, j)) break
+
+            exch(trav, j)
+
+            trav = j
+        }
+    }
+
+    private fun swim(k: Int) {
+        var trav = k
+
+        while (trav > 1 && less(trav / 2, trav)) {
+            exch(trav, trav / 2)
+            trav /= 2
+        }
+    }
+
+    private fun exch(i: Int, j: Int) {
+        val temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun less(lhs: Int, rhs: Int): Boolean = (array[lhs] as Comparable<T>) < (array[rhs] as T)
+
     override fun toString(): String = array.filter { it != null }.joinToString()
 }
 
 fun main(args: Array<String>) {
-    val maxPQ = MaxPQ<Int>(20)
 
-    val list = mutableListOf(16, 14, 2, 19, 13, 18, 1, 10, 5, 4, 3, 6, 20, 7, 9, 11, 12, 15, 17, 8)
+    val maxPQ = MaxPQ<Int>(100)
 
-    list.shuffle()
+    val list = mutableListOf(9, 2, 3, 8, 1, 10, 6, 17, 5, 16, 18, 12, 14, 11, 15, 20, 7, 4, 19, 13)
 
     for (i in 0 until 20) {
         maxPQ.add(list[i])
